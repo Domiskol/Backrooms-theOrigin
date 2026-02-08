f608f3f7ac2830ebec3f986aa060a313625d68fa
domin <dominik.saidl@seznam.cz>
2026-02-08 13:28:45 +0100

    Command a konzole tridy

---
+++ src/Command.java
@@ -0,0 +1,17 @@
+
+
+public abstract class Command {
+
+    protected String prikaz;
+
+    public void setPrikaz(String prikaz) {
+        this.prikaz = prikaz;
+    }
+
+    public abstract String execute();
+
+    public abstract boolean exit();
+}
+
+
+
